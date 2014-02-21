810a2c980aaa778645203ade0132c0855e4a9cc8
liyou <winxpliyou@163.com>
2014-02-21 17:08:51 +0800

    add a endl macro for debug_print

--- libCC/cc_debug_print.h
+++ libCC/cc_debug_print.h
@@ -6,40 +6,53 @@
 #include <stdio.h>
 #ifdef CC_PRINT_WARNING
 #define cc_print_warning(x) printf((x))
+#define cc_print_warning_endl() printf("\n")
 #else
 #define cc_print_warning(x)
+#define cc_print_warning_endl()
 #endif
 #ifdef CC_PRINT_ERROR
 #define cc_print_error(x) printf((x))
+#define cc_print_error_endl() printf("\n")
 #else
 #define cc_print_error(x) printf((x))
+#define cc_print_error_endl()
 #endif
 #ifdef CC_PRINT_TEST
 #define cc_print_test(x) printf((x))
+#define cc_print_test_endl() printf("\n")
 #else
 #define cc_print_test(x) printf((x))
+#define cc_print_test_endl()
 #endif
+
 
 #else
 
 #include <iostream>
 #ifdef CC_PRINT_WARNING
 #define cc_print_warning(x) std::cout<<(x)
+#define cc_print_warning_endl() std::cout<<std::endl
 #else
 #define cc_print_warning(x) 
+#define cc_print_warning_endl() 
 #endif 
 
 #ifdef CC_PRINT_ERROR
 #define cc_print_error(x) std::cout<<(x)
+#define cc_print_error_endl() std::cout<<std::endl
 #else
 #define cc_print_error(x) 
+#define cc_print_error_endl()
 #endif 
 
 
 #ifdef CC_PRINT_TEST
 #define cc_print_test(x) std::cout<<(x)
+#define cc_print_test_endl() std::cout<<std::endl;
 #else
 #define cc_print_test(x) 
+#define cc_print_test_endl()
 #endif
  
 #endif
---
+++ src/test.cpp
@@ -0,0 +1,13 @@
+#include "../libCC/cc_debug_print.h"
+
+#include <iostream>
+
+int main()
+{
+  cc_print_error("hello");
+  cc_print_error("world");
+  cc_print_error_endl();
+  cc_print_error("second line");
+  return 0;
+
+}
