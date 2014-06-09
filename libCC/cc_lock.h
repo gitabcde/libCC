a20a95665fe541bd11b6505c352da9cb7be90bbd
liyou <wimxpliyou@gmail.com>
2014-06-09 22:52:17 +0800

    fix the error for the mutex's typedef

--- libCC/cc_lock.h
+++ libCC/cc_lock.h
@@ -2,16 +2,16 @@
 #define CC_LOCK_H_
 
 #ifdef _WIN32_
-typedef CRITICAL_SECTION cc_critical
 #include <windows.h>
 #include <process.h>
+typedef CRITICAL_SECTION cc_critical;
 #define CC_CreateCritical(a) InitializeCriticalSection(a)
 #define CC_EnterCritical(a) EnterCriticalSection(a)
 #define CC_LeaveCritical(a) LeaveCriticalSection(a)
 #define CC_DeleteCritical(a) DeleteCriticalSection(a) 
 #elif (defined(__unix__) || defined(__APPLE__))
-typedef pthread_mutex_t cc_critical
 #include <pthread.h>
+typedef pthread_mutex_t cc_critical;
 #define CC_CreateCritical(a) pthread_mutex_init(a,NULL)
 #define CC_EnterCritical(a) pthread_mutex_lock(a)
 #define CC_LeaveCritical(a) pthread_mutex_unlock(a)
